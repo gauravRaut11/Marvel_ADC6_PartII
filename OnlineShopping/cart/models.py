a28662b9c944b0d23189b14a0a3f09051215e475
gauravRaut11 <59007224+gauravRaut11@users.noreply.github.com>
2020-02-02 20:56:24 +0545

    commit2

--- OnlineShopping/cart/models.py
+++ OnlineShopping/cart/models.py
@@ -2,17 +2,27 @@ from django.db import models
 from index.models import Item
 from django.contrib.auth.models import User
 # Create your models here.
-class Order(models.Model):
-    products = models.OneToOneField(Item, on_delete=models.SET_NULL, null=True)
-
-    def __str__(self):
-        return self.products.item_name
-
-class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    items = models.ManyToManyField(Order)
+class OrderItem(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.SET_NULL, null=True)
+    item = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True)
    
+    def __str__(self):
+        return  self.item.product_name
 
-    def get_cart_items(self):
-        return self.items.all()
+    def get_final_price(self):
+        return self.item.price
+
+class Order(models.Model):
+    items = models.ManyToManyField(OrderItem)
+    
+    def __str__(self):
+        return self.user.username
+
+    def get_total(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.get_final_price()
+        return total
+
 
--- OnlineShopping/cart/views.py
+++ OnlineShopping/cart/views.py
@@ -1,32 +1,22 @@
 
 from django.shortcuts import render
-from .models import Order, Cart
+from .models import Order, OrderItem
 from index.models import Item
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-def get_user_pending_order(request):
-    user_profile = get_object_or_404(User, username=request.user)
-    order = Cart.objects.filter(user=user_profile)
-    if order.exists():
-        return order[0]
 
-    return 0
-
-
-def add_to_cart(request,pk):
-    user_profile = get_object_or_404(User, username=request.user)
-    products = Item.objects.get(pk=pk)
-    order_item, status = Order.objects.get_or_create(products=products)
-    user_order, status = Cart.objects.get_or_create(user=user_profile)
-    user_order.items.add(order_item)
-    
-    if status:
-        user_order.save()
-    
-    return redirect(reverse('carts'))
-
+def add_to_cart(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user
+    )
+    order = Order.objects.create(
+        user=request.user)
+    order.items.add(order_item)
+    return redirect("carts")
 
 def delete_from_cart(request,id):
     item_to_delete = Order.objects.filter(pk=id)
@@ -36,7 +26,7 @@ def delete_from_cart(request,id):
 
 
 def cart_details(request):
-    show_order = get_user_pending_order(request)
+    show_order = Order.objects.get(user=request.user, ordered=False)
     context = {
         'order':show_order
     }
